8abdbf124248222239e8eac8ae270d25bfc047c3
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-21 21:57:28 -0300

    Update index.php

--- ex/teste04/index.php
+++ ex/teste04/index.php
@@ -32,5 +32,41 @@
     
     ?>
     </pre>
+
+    <p>
+    <?php 
+        /**
+         * Nowdocs estão para aspas simples assim como os heredocs estão para 
+         * aspas duplas em strings. Um nowdoc é especificado similarmente a um 
+         * heredoc, mas nenhuma interpretação é feita dentro de um nowdoc. 
+         */
+    
+        echo <<<'END'
+        <pre>
+        Exemplo de string abrangendo várias linhas
+        usando a sintaxe nowdoc. As barras invertidas são sempre tratadas literalmente,
+        por exemplo. \\ e \'.
+        Isso é bom para grandes blocos de texte e que contem um código PHP.
+        copiando o código que gerou este texto: 
+
+        < ? p h p
+        /**
+         * Nowdocs estão para aspas simples assim como os heredocs estão para 
+         * aspas duplas em strings. Um nowdoc é especificado similarmente a um 
+         * heredoc, mas nenhuma interpretação é feita dentro de um nowdoc. 
+         */
+    
+        echo <<<'END'
+        Exemplo de string abrangendo várias linhas
+        usando a sintaxe nowdoc. As barras invertidas são sempre tratadas literalmente,
+        por exemplo. \\ e \'.
+        Isso é bom para grandes blocos de texte e que contem um código PHP.
+        ~END;
+        ?>
+        </pre>
+        END;
+    ?>
+    </p>
+
 </body>
 </html>

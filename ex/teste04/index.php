5cbbcd2d223b34bc0d727c8bf890965720c8504b
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-21 21:18:24 -0300

    Create index.php

---
+++ ex/teste04/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP string </title>
+</head>
+<body>
+    
+    <?php 
+        // Imprime: Arnold disse uma vez: "I'll be back"
+        echo 'Arnold disse uma vez: "I\'ll be back."';
+        // Imprime: Contra barra \
+        echo '<br>Contra barra \\';
+        // Imprime: Use tag br para quebrar a linha ao inves de \n.
+        echo '<br>Use tag br para quebrar a linha ao inves de \\n.'
+    ?>
+
+
+
+</body>
+</html>

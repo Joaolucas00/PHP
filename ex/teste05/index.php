37578e0b27f7802c6935042f1620ac8b210d0a34
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-21 23:03:07 -0300

    Update index.php

--- ex/teste05/index.php
+++ ex/teste05/index.php
@@ -25,6 +25,15 @@
             "idade" => 18,
         ];
         var_dump($outro_exempro_array);
+
+        /** Informações úteis
+         * strings contendo quaisquer ints válidos, serão convertidos para o tipo int. Por exemplo, a chave"8" será, na verdade, armazenada como 8. Entretanto, "08" não será convertido, por não ser um inteiro decimal válido.
+         * floats também são convertidos para ints, isso significa que a parte fracionária será removida. Por exemplo, a chave 8.7 será na verdade armazenada como 8.
+         * bools são convertidos para ints, igualmente, por exemplo, a chave true, será na verdade armazenada como 1 e a chave false como 0.
+         * null será convertido para uma string vazia, por exemplo, a chave null na verdade será armazenada como "".       
+         * arrays e objects não podem ser usados como chaves. Fazer isso resultará em um aviso: Illegal offset type.  
+         */
+
     ?>   
     </pre> 
 </body>

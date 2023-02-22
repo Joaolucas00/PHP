cde6c831d3037c3f970a17e92ae8a225945a2c57
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-21 21:33:11 -0300

    Update index.php

--- ex/teste04/index.php
+++ ex/teste04/index.php
@@ -13,11 +13,24 @@
         echo 'Arnold disse uma vez: "I\'ll be back."';
         // Imprime: Contra barra \
         echo '<br>Contra barra \\';
-        // Imprime: Use tag br para quebrar a linha ao inves de \n.
-        echo '<br>Use tag br para quebrar a linha ao inves de \\n.'
+        // Imprime: Use tag br para quebrar a linha em vez de \n.
+        echo '<br>Use tag br para quebrar a linha em vez de \\n.';
+        // Imprime: Variáveis não funcionam dentro de aspas simples pois não é interpretado
+        echo '<br>Variáveis não funcionam dentro de aspas simples pois não é interpretado';
     ?>
-
-
-
+    <pre>
+    <?php // Usando heredoc com vetor
+        $values = [<<<END
+        a
+          b
+            c
+        END, 'd e f'];
+        echo "<br>";
+        
+        var_dump($values);
+    
+    
+    ?>
+    </pre>
 </body>
 </html>

bb6d4fb3f1b6002e98d5251cc8fcd980a63f1e70
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-26 22:05:17 -0300

    Update index.php

--- ex/teste07/index.php
+++ ex/teste07/index.php
@@ -16,12 +16,51 @@
     </style>
 </head>
 <body>
+    <?php function fim() {echo "<br>"; echo "<hr>";}?>
     <h1>Variável por referência</h1>
     <?php 
         $nome = 'João'; //atribui João a variável $nome
         $var = &$nome; // Referencia $nome através de $var
         $var = "Meu nome é $var"; // Altera $var, e também altera $nome.
         echo $var . " --> variável \$var <br>"; echo $nome . " --> variável \$nome";
+
+        // Apenas varáveis nomeadas podem ser atribuída
+        fim();
+    ?>
+
+    <h1>Escopo</h1>
+    <?php 
+        $n1 = 1; // escopo global
+        $n2 = 2;
+
+        function soma() { // escopo local
+            global $n1, $n2; 
+            // Declarando n1 e n2 como globaism, fará com que todas as referências a essas variáveis referênciam a versão global
+            // Significa que isso vai alterar a variável global também, mesmo sendo local.
+
+            $n2 = $n1 + $n2;
+        }
+
+        soma();
+        echo $n2;
+        // Outra maneira de fazer com $GLOBALS
+        // $GLOBALS existe em qualquer escopo, porque ela é uma superglobal
+
+        $n3 = 3;
+        $n4 = 4;
+
+        function sum() {
+            $GLOBALS['n4'] = $GLOBALS['n3'] + $GLOBALS['n4'];
+        }
+        sum();
+        echo $n4;
+        fim();
+    ?>
+    <h1>Static</h1>
+    <?php 
+        
+    
+    
     ?>
 </body>
 </html>

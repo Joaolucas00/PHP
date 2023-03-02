73df08fd8d48910fe04bd659b826c73d45c93228
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-02 20:11:10 -0300

    Update index.php

--- ex/teste10/index.php
+++ ex/teste10/index.php
@@ -4,13 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operador ternário</title>
+    <title>Operador ternário e lógicos</title>
 </head>
 <body>
     <?php 
+        //alguns testes com operadores de Comparação
+        var_dump("1" == "01"); // 1 == 1 -> true
+        var_dump("10" == "1e1"); // 10 == 10 -> true
+        var_dump(100 >= "1e2"); // 100 == 100 -> true
 
-    
-    
+
+        $a = (empty($_REQUEST['exemplo'])) ? null: $_REQUEST['exemplo'];
+        var_dump($a);
+
+        // a expressão acima é idêntico a este if/else abaixo
+
+        if (empty($_REQUEST['exemplo'])) {
+            $a = null;
+        } else {
+            $a = $_REQUEST['exemplo'];
+        }
+        var_dump($a);
+
+        // mais de um operador ternário ao mesmo tempo
+        echo ((true?'true':false)?'a':'b'); // Imprime: a 
+        // é avaliado da esquerda para a direita, mas é bom colocar os parênteses para facilitar a leitura do código
+
+        // operador de coalescência nula (??)
+        $exemplo = $_REQUEST['exemplo'];
     ?>
 </body>
 </html>

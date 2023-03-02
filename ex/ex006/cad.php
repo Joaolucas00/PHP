a4a8bdf33d19db993450742d56226b5fb572e7a6
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-02 15:43:04 -0300

    modificando o cad.php

--- ex/ex006/cad.php
+++ ex/ex006/cad.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
+    <link rel="stylesheet" href="estilo.css">
+    <style>
+        p {
+            color: white;
+        }
+
+    </style>
 </head>
 <body>
-    
+    <?php 
+        $nome = $_GET["nome"];
+        $sobrenome = $_GET["sobrenome"];
+    ?>
+    <main>
+    <header>
+        <h1>Olá, <?php 
+                echo "$nome!";
+        ?></h1>
+    </header>
+    <section>
+        <p><?php 
+            echo "É um prazer te conhecer, $nome $sobrenome"
+        ?>    
+        </p>
+    </section>
+    </main>
 </body>
 </html>

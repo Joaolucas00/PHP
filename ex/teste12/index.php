842333f85cb9d5d18587d55efc124f054f7bceab
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-05 18:39:54 -0300

    Create index.php

---
+++ ex/teste12/index.php
@@ -0,0 +1,41 @@
+<?php 
+
+    // testando json
+    $json_str = '{"nome": "J", "idade": 32, "sexo": "M"}';
+    $obj = json_decode($json_str);
+    // json_decode recebe como entrada uma strig codificada no formato JSON
+    // json_decode tranforma o JSON em um objeto php
+
+    var_dump($json_str);
+    echo "<br>";
+    var_dump($obj);
+    echo "<br>";
+    echo "nome: $obj->nome <br>";
+    echo "idade: $obj->idade <br>";
+    echo "sexo: $obj->sexo <br>";
+
+    echo "<hr>";
+    // JSON com múltiplos dados
+
+    // string json com 3 elementos
+    $json_mul_str = '{"pessoas": '. 
+        '[{"nome": "João", "idade": 18, "sexo": "M"}, ' .
+        ' {"nome": "Lucas", "idade": 19, "sexo": "M"}, ' .
+        ' {"nome": "Luan", "idade": 20, "sexo": "M"} ' . 
+        ']}'; 
+
+    $objetoJson = json_decode($json_mul_str);
+
+    $galera = $objetoJson->pessoas;
+    echo "<pre>";
+    var_dump($json_mul_str);
+    echo "\$json_mul_str<br>";
+    var_dump($objetoJson);
+    echo "\$objetoJson <br>";
+    var_dump($galera);
+    echo "\$galera<br>";
+    echo"</pre>";
+    foreach ($galera as $g) {
+        echo "nome: $g->nome - idade: $g->idade - sexo: $g->sexo -";
+    }
+?>

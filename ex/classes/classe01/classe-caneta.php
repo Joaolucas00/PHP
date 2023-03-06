38345127269a760d9a7e374c788da5506451270d
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-06 20:19:47 -0300

    ...

--- ex/classes/classe01/classe-caneta.php
+++ ex/classes/classe01/classe-caneta.php
@@ -1,14 +1,23 @@
 <?php 
     // No PHP você pode botar variás classes no mesmo arquivo, diferente de Java.
     class Caneta {
+
+        // declaração de atributo ou propriedade
+
         public string $modelo;
         public $cor;
         var $ponta; // public por padrão
         public int $carga;
         public bool $tampada;
 
+        // declaração de método
         function rabiscar() {
-            echo "Rabiscando";
+            if ($this->tampada) {
+                echo "A caneta está tampada..";
+            } else {
+                echo "Rabiscando..";
+            }
+
         }
 
         function tampar() {
--- ex/classes/classe01/index.php
+++ ex/classes/classe01/index.php
@@ -8,7 +8,16 @@
 </head>
 <body>
     <?php 
-    
+        include_once 'classe-caneta.php';
+
+        $c1 = new Caneta; // se não há argumentos a ser passado para o construtor, o parênteses é opcional.
+
+        $c1->cor = "Vermelho";
+        $c1->ponta = 0.5;
+        $c1->carga = 90;
+        $c1->tampada = true;
+
+        $c1->rabiscar();
     
     
     ?>

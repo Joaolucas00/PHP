b20aa9a64fae69a73e650057a2fdc02b53c3eaa1
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-06 20:09:29 -0300

    Update classe-caneta.php

--- ex/classes/classe01/classe-caneta.php
+++ ex/classes/classe01/classe-caneta.php
@@ -1,12 +1,26 @@
 <?php 
     // No PHP você pode botar variás classes no mesmo arquivo, diferente de Java.
     class Caneta {
+        public string $modelo;
+        public $cor;
+        var $ponta; // public por padrão
+        public int $carga;
+        public bool $tampada;
 
+        function rabiscar() {
+            echo "Rabiscando";
+        }
+
+        function tampar() {
+            $this->tampada = true;
+        }
+
+        function destampar() {
+            $this->tampada = false;
+        }
     }
 
-    class Pessoa {
-        
-    }
+    class Pessoa {}
 
 
 

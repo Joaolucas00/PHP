6903ab7f9cb54b33f8d9d5031211e9bccae62b07
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-08 20:57:45 -0300

    .....

--- ex/classes/classe03/classes.php
+++ ex/classes/classe03/classes.php
@@ -1,6 +1,48 @@
 <?php 
     // Construtores pais não são chamados implicitamente se a classe filha define um construtor. Para executar o construtor da classe pai, uma chamada a parent::__construct() dentro do construtor da classe filha é necessária. Se a classe filha não definir um construtor, será herdado da classe pai como um método normal (se não foi declarado como privado).
 
+    class ClassePai {
+        function __construct()
+        {
+            echo "Classe pai";
+        }
+    }
+
+    class ClasseFilha extends ClassePai {
+        function __construct() 
+        {
+            parent::__construct(); // chama o construtor da ClassePai
+            echo "Classe filha";
+        }
+    }
+
+    class subClass extends ClassePai {
+        // Construtor da ClassePai herdado
+    }
+
+
+    // Utilizando argumentos de construtor
+    class CLasseExemplo {
+        protected int $x;
+        protected int $y;
+
+        public function __construct(int $x, int $y = 0)
+        {
+            $this->x = $x;
+            $this->y = $y;
+        }
+    }
+
+    // Utilizando promoção de propriedades no construtor
+
+    class Classe {
+        public function __construct(public int $x, public int $y = 0)
+        {
+            $this->x = $x;
+            $this->y = $y;
+        }
+    }
+
 
 
 ?>
--- ex/classes/classe03/index.php
+++ ex/classes/classe03/index.php
@@ -7,7 +7,28 @@
     <title>Construtores PHP</title>
 </head>
 <body>
+    <?php
+        include 'classes.php'; 
+
+        $paiCLass = new ClassePai();
+        $filhaClass = new ClasseFilha(); 
+        $subClass = new subClass();
 
 
+        // passagens de ambos os argumentos
+        $e1 = new CLasseExemplo(0, 2);
+
+        // passagem de um argumento obrigatório, y padrão zero
+        $e2 = new CLasseExemplo(5);
+
+        // com parâmetro nomeados (a partir do PHP 8.0)
+        $e3 = new CLasseExemplo(y: 3, x: 4);
+
+        $c1 = new Classe(4);
+        echo $c1->x;
+        echo $c1->y;
+
+
+    ?>
 </body>
 </html>

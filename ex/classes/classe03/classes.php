1e2629cee117f53a296f459d2c0a36fc1170150c
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-08 19:38:01 -0300

    .

---
+++ ex/classes/classe03/classes.php
@@ -0,0 +1,9 @@
+<?php 
+    class Classe01 {const ID1 = '1'; public $mID = fn() => self::ID1;}
+
+    class Classe02 {const ID2 = '2'; public $mID = fn() => self::ID2;}
+
+    class Classe03 {const ID3 = '3'; public $mID = fn() => self::ID3;}
+
+
+?>

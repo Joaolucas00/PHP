9a2755f6aa65425349675c361adaea828bb88cf0
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-09 22:16:22 -0300

    .

--- ex/classes/classe09/index.php
+++ ex/classes/classe09/index.php
@@ -8,30 +8,44 @@
 </head>
 <body>
     <?php 
-    /** Interfaces de objetos permitem a criação de códigos que especificam quais métodos uma classe deve implementar, 
-     * sem definir como esses métodos serão tratados.
-     * Interfaces são definidas da mesma forma que classes, 
-     * mas com a palavra-chave interface substituindo class 
-     * e com nenhum dos métodos tendo seu conteúdo definido.
-     * 
-     * Todos os métodos declarados em uma interface devem ser públicos, essa é a natureza de uma interface.
-     * 
-     *  Classes podem implementar mais de uma interface se assim for desejado, separando cada interface com uma vírgula.
-     * 
-     *  Interfaces podem ser estendidas como as classes, usando o operador extends.
-     * 
-     *  A classe que implementa a interface precisa ter as mesmas assinaturas de método como definidas na interface. Não fazê-lo resultará em um erro fatal
-     * 
-     *  Em versões anteriores ao PHP 5.3.9, uma classe não poderia implementar duas interfaces que especificassem um método com o mesmo nome, pois causaria ambiguidade. 
-     * Versões mais recentes do PHP, isso é permitido, desde que os métodos duplicados tenham a mesma assinatura.
-     * 
-     *  Constantes
-     * É possível ter constantes em interfaces. 
-     * Constantes de interfaces funcionam exatamente como constantes de classes, 
-     * com exceção de não poderem ser sobrescritas por uma classe/interface herdeira.
-     */
+        /** Interfaces de objetos permitem a criação de códigos que especificam quais métodos uma classe deve implementar, 
+         * sem definir como esses métodos serão tratados.
+         * Interfaces são definidas da mesma forma que classes, 
+         * mas com a palavra-chave interface substituindo class 
+         * e com nenhum dos métodos tendo seu conteúdo definido.
+         * 
+         * Todos os métodos declarados em uma interface devem ser públicos, essa é a natureza de uma interface.
+         * 
+         *  Classes podem implementar mais de uma interface se assim for desejado, separando cada interface com uma vírgula.
+         * 
+         *  Interfaces podem ser estendidas como as classes, usando o operador extends.
+         * 
+         *  A classe que implementa a interface precisa ter as mesmas assinaturas de método como definidas na interface. Não fazê-lo resultará em um erro fatal
+         * 
+         *  Em versões anteriores ao PHP 5.3.9, uma classe não poderia implementar duas interfaces que especificassem um método com o mesmo nome, pois causaria ambiguidade. 
+         * Versões mais recentes do PHP, isso é permitido, desde que os métodos duplicados tenham a mesma assinatura.
+         * 
+         *  Constantes
+         * É possível ter constantes em interfaces. 
+         * Constantes de interfaces funcionam exatamente como constantes de classes, 
+         * com exceção de não poderem ser sobrescritas por uma classe/interface herdeira.
+         */
     
-    
+        interface Interfac {
+            public function funcao();
+            public const CONSTANTE = "Constante";
+        }
+
+        class C implements Interfac {
+            public function funcao () {return self::CONSTANTE;}
+        }
+
+        $obj = new C;
+        print C::CONSTANTE;
+        print $obj->funcao();
+
+        
+
     
     
     ?>

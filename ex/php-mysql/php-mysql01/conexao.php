b27c5bbe924af915ec71d13990d442eae6fc133b
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-19 22:49:44 -0300

    Update conexao.php

--- ex/php-mysql/php-mysql01/conexao.php
+++ ex/php-mysql/php-mysql01/conexao.php
@@ -9,6 +9,17 @@
             echo "Falha ao conectar: ( " . $mysqli->connect_errno . ") ";
         }
         echo "<h1>Olá..</h1>";
+
+        $sql = "SELECT * FROM gafanhotos";
+        $resultado = $mysqli->query($sql);
+
+        if ($resultado->num_rows > 0) {
+            while ($row = $resultado->fetch_assoc()) { //Obtem uma linha do conjunto de resultados como uma matriz associativa
+                echo " - curso preferido:  " . $row["curso_preferido"] . " - id :" . $row["id"] . " - Nome: " . $row["nome"] . "- profissao: " . $row["profissao"] . " - nascimento: " . $row["nascimento"] . "- sexo: " . $row["sexo"] . "- peso: " . $row["peso"] . "- altura: " . $row["altura"] . "- nacionalidade: " . $row["nacionalidade"] . "<br>";
+            }
+        }
+        $resultado->close(); // Fecha uma conexão de banco de dados aberta anterio
+
         
         
 

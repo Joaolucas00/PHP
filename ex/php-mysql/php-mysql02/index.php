262c461ed18602104a3c9a53f6f82498391902fe
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-21 19:16:57 -0300

    .

--- ex/php-mysql/php-mysql02/index.php
+++ ex/php-mysql/php-mysql02/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inserindo dados no banco de dados com php</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     <?php 
@@ -12,5 +13,12 @@
         // inserindo dados
     
     ?>
+    <form action="" method="get">
+        <label for="inome">nome: </label>
+        <input type="text" name="nome" id="inome"> <br>
+        <label for="curso_"></label>
+        <input type="submit" value="Cadastrar" id="in_submit">
+    </form>
+
 </body>
 </html>

499527da542d3431375b2d491b5f077fa41a1c4c
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-21 19:32:57 -0300

    ..

--- ex/php-mysql/php-mysql02/index.php
+++ ex/php-mysql/php-mysql02/index.php
@@ -16,7 +16,20 @@
     <form action="" method="get">
         <label for="inome">nome: </label>
         <input type="text" name="nome" id="inome"> <br>
-        <label for="curso_"></label>
+        <label for="icurso_pref">curso preferido</label>
+        <input type="number" name="curso_pref" id="icurso_pref"> <br>
+        <label for="iprofissao">profissao</label>
+        <input type="text" name="profissao" id="iprofissao"> <br>
+        <label for="inasc">nascimento</label>
+        <input type="date" name="nasc" id="inasc"> <br>
+        <label for="isexo">sexo:</label> <br>
+        <label for="iM">Masculino</label>
+        <input type="radio" name="sexo" id="iM" checked value="M">
+        <label for="iF">Feminino</label>
+        <input type="radio" name="sexo" id="iF" value="F">
+
+
+        <br>
         <input type="submit" value="Cadastrar" id="in_submit">
     </form>
 

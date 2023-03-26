0f6398556352fe0ae4cfb006e648dbdc84b6b78a
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-03-26 20:43:54 -0300

    Update conexao.php

--- ex/php-mysql/php-mysql04/conexao.php
+++ ex/php-mysql/php-mysql04/conexao.php
@@ -1,4 +1,8 @@
 <?php 
+
+    // Declarações Preparadas e Parâmetros Vinculados
+
+
     $server = "localhost";
     $user = "root";
     $senha = '';
@@ -11,11 +15,31 @@
         echo "Erro ao conectar";
     }
 
-    $stmt = $banco_de_dados->prepare("INSERT INTO gafanhotos (curso_preferido, nome) VALUES (?, ?)");
-    $stmt->bind_param("is", $cp, $nome);
+    // prepare e bind
 
+    $stmt = $banco_de_dados->prepare("INSERT INTO gafanhotos (curso_preferido, nome) VALUES (?, ?)");
+    // ? é o valor que vai ser informado após o bind_param
+
+    $stmt->bind_param("is", $cp, $nome);
+    /** Essa função vincula os parâmetros à consulta SQL e informa ao banco de dados quais são os parâmetros. 
+     * O argumento "sss" lista os tipos de dados que são os parâmetros. 
+     * O caractere s diz ao mysql que o parâmetro é uma string.
+     * 
+     *  O argumento pode ser um dos quatro tipos:
+     * i - integer
+     * d - double
+     * s - string
+     * b - BLOB
+     * 
+     *  Ao dizer ao mysql que tipo de dados esperar, minimizamos o risco de injeções de SQL.
+     */
+
+    // define parâmetros e executa
     $cp = 5;
     $nome = "Alison";
     $stmt->execute();
+
+    
+
 
 ?>

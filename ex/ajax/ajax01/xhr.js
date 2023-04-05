f6138d26aeb96fd27657a259fccaadf29ae962d2
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-04-05 00:00:23 -0300

    Update xhr.js

--- ex/ajax/ajax01/xhr.js
+++ ex/ajax/ajax01/xhr.js
@@ -1,2 +1,13 @@
 var xhr = new XMLHttpRequest();
-console.log(xhr);
+
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4) {
+        console.log(xhr);
+        console.log("OK");
+    }
+}
+
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
+
+xhr.send();

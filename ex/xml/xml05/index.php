0e9dd8cf531dad9454884388fd4affd1dd7c7e00
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-04-07 21:37:32 -0300

    Update index.php

--- ex/xml/xml05/index.php
+++ ex/xml/xml05/index.php
@@ -32,8 +32,12 @@
             }
         }
 
-        function char ($parser, $data) {
-            echo $data;
+        function stop($parser, $element_name) {
+            echo "<br>";
+        }
+
+        function char ($parser, $dado) {
+            echo $dado;
         }
 
         // Configurara manipuladores de elemento inicial e final
@@ -42,6 +46,19 @@
         //  Define a função do manipulador de dados de caractere para o analisador do analisador XML.
         xml_set_character_data_handler($parser, "char");
 
+        // Abrindo arquivo XML
+        $xml_aberto = fopen("dados.xml", "r");
+
+        // Lendo dado
+        // fread() lê até length bytes do ponteiro de arquivo informado em handle. A leitura é interrompida quando uma das seguintes condições são satisfeitas:
+        while ($dado = fread($xml_aberto, 4096)) {
+            //  Inicia a análise em um documento XML
+            xml_parse($parser,$dado,feof($xml_aberto)) or
+            die (sprintf("XML Error: %s at line %d",
+            xml_error_string(xml_get_error_code($parser)),
+            xml_get_current_line_number($parser)));
+        }
+        xml_parser_free($parser);
 
 
 
@@ -51,9 +68,13 @@
          * 
          * 2 - Crie funções para usar com os diferentes manipuladores de eventos
          * 3 - Adicione a função xml_set_element_handler() para especificar qual função será executada quando o analisador encontrar as tags de abertura e fechamento
+         * 
          * 4 - Adicione a função xml_set_character_data_handler() para especificar qual função será executada quando o analisador encontrar dados de caracteres
-         * 5 - Analise o arquivo "note.xml" com a função xml_parse()
+         * 
+         * 5 - Analise o arquivo "dados.xml" com a função xml_parse()
+         * 
          * 6 - Em caso de erro, adicione a função xml_error_string() para converter um erro XML em uma descrição textual
+         * 
          * 7 - Chame a função xml_parser_free() para liberar a memória alocada com a função xml_parser_cre
          * 
          * 

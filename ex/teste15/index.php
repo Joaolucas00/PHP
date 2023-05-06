f51ee4c5f2af38edff65ae0596a0751bb2fa25c4
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-06 19:18:18 -0300

    Create index.php

---
+++ ex/teste15/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test 15</title>
+</head>
+<body>
+    <?php
+
+use Classe2 as GlobalClasse2;
+
+        interface i {
+            public function getAtributo();
+        }
+    
+        class Classe1 implements i {
+            private $atributo;
+            public function __construct(string $atributo)
+            {
+                $this->atributo = $atributo;
+            }
+
+            public function getAtributo() {
+                return $this->atributo;
+            }
+        }
+
+        class Classe2 {
+            public static function returnClasse1 (string $str) {
+                return new Classe1("Olá, " . $str);
+            }
+        }
+
+        $obj = new Classe2;
+
+        $obj2 = $obj->returnClasse1("Mundo!");
+        echo $obj2->getAtributo();
+        echo "<hr>";
+        var_dump($obj);
+        echo "<hr>";
+        var_dump($obj2);
+    
+    
+    ?>
+</body>
+</html>
